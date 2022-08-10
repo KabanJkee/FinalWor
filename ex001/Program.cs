ef0fc53e98593365d6a943bb736f4574fd26df5b
KabanJkee <KabanJkee@gmail.com>
2022-08-10 20:09:36 +0300

    add ex001

---
+++ ex001/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Написать программу, которая из имеющегося массива строк 
+формирует новый массив из строк, длина которых меньше, 
+либо равна 3 символам. Первоначальный массив можно ввести 
+с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше 
+обойтись исключительно массивами.
+*/
+
+            string[] array;
+            Console.WriteLine("Введите массив строк через пробел");
+            string input = Console.ReadLine();
+            array = input.Split(' ');
+            string [] result = new string[array.Length];
+            int i = 0;
+            foreach (string value in array)
+            {
+                if (value.Length <= 3)
+                {
+                    result[i] = value;
+                    i++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, i));
